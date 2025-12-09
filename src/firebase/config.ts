1e3a3f2506bdceb47c338c0bf78d39272a872379
Austin Patterson <austin@catalyst.com.co>
2025-12-09 15:23:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2557083098-2faf8",
+  "appId": "1:987211671256:web:3b0da8713592e006edd9ad",
+  "apiKey": "AIzaSyBAJLae_eFL2VgdXCXbiXoJwYmcIM0mLE8",
+  "authDomain": "studio-2557083098-2faf8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "987211671256"
+};
