4a7f67ba22692c14dbd6a43e0f8ce7ba749c80af
Austin Patterson <austin@catalyst.com.co>
2025-12-09 23:32:44 +0000

    Is the app migrated?

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,9 @@
 
 export const firebaseConfig = {
-  "projectId": "studio-2557083098-2faf8",
-  "appId": "1:987211671256:web:3b0da8713592e006edd9ad",
-  "storageBucket": "studio-2557083098-2faf8.appspot.com",
-  "apiKey": "AIzaSyBAJLae_eFL2VgdXCXbiXoJwYmcIM0mLE8",
-  "authDomain": "studio-2557083098-2faf8.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "987211671256"
+  "projectId": "pxl8-final",
+  "appId": "1:368146953267:web:e41d8c557359b3c46a67a2",
+  "storageBucket": "pxl8-final.appspot.com",
+  "apiKey": "AIzaSyAz4o4o_xV4Y2h6b7c8d9e0f1g2h3i4j5",
+  "authDomain": "pxl8-final.firebaseapp.com",
+  "measurementId": "G-MEASUREMENT_ID"
 };
