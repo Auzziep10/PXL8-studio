de7be9740194e0334c3df5c594b9a5e7be111edb
Austin Patterson <austin@catalyst.com.co>
2025-12-09 23:47:06 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 
 export const firebaseConfig = {
   "projectId": "pxl8-final",
-  "appId": "1:368146953267:web:e41d8c557359b3c46a67a2",
+  "appId": "1:1052987187294:web:6b684f33782eee16d1d4cf",
   "storageBucket": "pxl8-final.appspot.com",
-  "apiKey": "AIzaSyAz4o4o_xV4Y2h6b7c8d9e0f1g2h3i4j5",
+  "apiKey": "AIzaSyDCupluD0NchkXYmmazuJY-NnUMZtyy5BA",
   "authDomain": "pxl8-final.firebaseapp.com",
   "measurementId": "G-MEASUREMENT_ID"
 };
