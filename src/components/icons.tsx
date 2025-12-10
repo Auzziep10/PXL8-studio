0ef18d4cd6791a5bf796138c896f55a748ea11e0
Austin Patterson <austin@catalyst.com.co>
2025-12-10 19:57:25 +0000

    Can you use this image for the logo now https://firebasestorage.googleap

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -11,7 +11,7 @@ export function PXL8Logo(props: SVGProps<SVGSVGElement>) {
 
   return (
     <NextImage
-      src="https://firebasestorage.googleapis.com/v0/b/studio-2557083098-2faf8.firebasestorage.app/o/PXL8%20logo%202%40300x.png?alt=media&token=606f53d5-7892-455f-b620-25dfaec5e752"
+      src="https://firebasestorage.googleapis.com/v0/b/pxl8-final.firebasestorage.app/o/website-images%2FPXL8%20logo%205%40300x.png?alt=media&token=6e9c6211-4e80-4b03-8ab1-22d5747eda46"
       alt="PXL8 Logo"
       width={Number(width)}
       height={Number(height)}
