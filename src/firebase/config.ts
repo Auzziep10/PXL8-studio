f35c132db3aadab1d1b7a6ec9326eb35d34f96b8
Austin Patterson <austin@catalyst.com.co>
2025-12-10 00:00:00 +0000

    This is all I see

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@
 export const firebaseConfig = {
   "projectId": "pxl8-final",
   "appId": "1:1052987187294:web:6b684f33782eee16d1d4cf",
-  "storageBucket": "pxl8-final.appspot.com",
+  "storageBucket": "pxl8-final.firebasestorage.app",
   "apiKey": "AIzaSyDCupluD0NchkXYmmazuJY-NnUMZtyy5BA",
   "authDomain": "pxl8-final.firebaseapp.com",
   "measurementId": "G-MEASUREMENT_ID"
