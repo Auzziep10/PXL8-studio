c94778e23e5f448a4787f7a833d0dc4579444941
Austin Patterson <austin@catalyst.com.co>
2025-12-12 15:35:52 +0000

    Also where's my new logo for the updated color scheme is in that folder:

--- src/components/ui/icons.tsx
+++ src/components/ui/icons.tsx
@@ -11,7 +11,7 @@ export function PXL8Logo(props: SVGProps<SVGSVGElement>) {
 
   return (
     <NextImage
-      src="https://firebasestorage.googleapis.com/v0/b/pxl8-final.appspot.com/o/website-images%2FPXL8%20logo%205%40300x.png?alt=media&token=6e9c6211-4e80-4b03-8ab1-22d5747eda46"
+      src="https://firebasestorage.googleapis.com/v0/b/pxl8-final.appspot.com/o/website-images%2Ff260e13b-710d-46b5-ab36-7f8fbd41bb8b?alt=media"
       alt="PXL8 Logo"
       width={Number(width)}
       height={Number(height)}
